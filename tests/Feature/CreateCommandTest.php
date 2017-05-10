22cf1c8ac682e23e44f14468a364e96a47c5217c
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-05-09 21:58:36 -0300

    Clean up tests

--- tests/Feature/CreateCommandTest.php
+++ tests/Feature/CreateCommandTest.php
@@ -12,12 +12,14 @@ class CreateVimCommandTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected $storeUrl = '/api/store';
+
     /** @test */
     function can_create_a_command()
     {
         $command = $this->makeCommand();
 
-        $this->post('/api/store', $command->toArray());
+        $this->post($this->storeUrl, $command->toArray());
 
         $this->assertDatabaseHas('commands', ['title' => $command->title]);
     }
@@ -25,37 +27,31 @@ class CreateVimCommandTest extends TestCase
     /** @test */
     function when_create_a_command_then_the_created_command_shold_be_returned_with_the_created_status_code()
     {
-        $command = $this->makeCommand();
+        $command = $this->makeCommand()->toArray();
 
-        $this->post('/api/store', $command->toArray())
+        $this->post($this->storeUrl, $command)
             ->assertStatus(201)
-            ->assertJsonFragment($command->toArray());
+            ->assertJsonFragment($command);
     }
 
     /** @test */
     function a_command_must_have_a_title()
     {
-        $command = $this->makeCommand(['title' => null]);
-
-        $this->post('/api/store', $command->toArray())
+        $this->post($this->storeUrl, $this->makeCommand(['title' => null])->toArray())
             ->assertSessionHasErrors(['title']);
     }
 
     /** @test */
     function a_command_must_have_a_command()
     {
-        $command = $this->makeCommand(['command' => null]);
-
-        $this->post('/api/store', $command->toArray())
+        $this->post($this->storeUrl, $this->makeCommand(['command' => null])->toArray())
             ->assertSessionHasErrors(['command']);
     }
 
     /** @test */
     function a_command_must_have_a_description()
     {
-        $command = $this->makeCommand(['description' => null]);
-
-        $this->post('/api/store', $command->toArray())
+        $this->post($this->storeUrl, $this->makeCommand(['description' => null])->toArray())
             ->assertSessionHasErrors(['description']);
     }
 
